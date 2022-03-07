f4cd1a341acc4e256fc642e4126e8a41461299ef
Andrea Sulova <44543964+sulova@users.noreply.github.com>
2022-03-07 11:59:11 +0100

    Update 1_SDB_Preprocessing.java

--- 1_SDB_Preprocessing.java
+++ 1_SDB_Preprocessing.java
@@ -6,9 +6,9 @@ Objectives: Multitemporal SDB
 
 //------------------- I N P U T S ------------------------/
 
-var date_start = '2021-12-01'
-var date_end = '2022-01-20'
-var cloud_percent = 40
+var date_start = '2022-02-15'
+var date_end = '2022-03-07'
+var cloud_percent = 100
 
 var geometry = ee.Geometry.Polygon([[[37.025169263255, 21.250398927417418],
           [37.025169263255, 20.927508858890256],[37.32180012263, 20.927508858890256],
@@ -22,7 +22,6 @@ Map.setOptions('HYBRID')
 // Cloud Mask
 function maskS2clouds(image) {
   var qa = image.select('QA60');
-  // Bits 10 and 11 are clouds and cirrus, respectively.
   var cloudBitMask = Math.pow(2, 10);
   var cirrusBitMask = Math.pow(2, 11);
   var mask = qa.bitwiseAnd(cloudBitMask).eq(0).and(
@@ -52,34 +51,25 @@ print(slider)
 
 var S2_composite = ee.ImageCollection('COPERNICUS/S2_SR')
                   .filterDate(date_start, date_end)
-                  .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE',cloud_percent))
+                  .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', cloud_percent))
                   .filterBounds(geometry)
                   .map(maskS2clouds)
                   //.map(landmask)
-                  
-print(S2_composite)
                  
 var dates = ee.List(S2_composite.aggregate_array("system:index")).map(function(d){return d});
-print('Number of images in collection:',S2_composite.size(), dates);
+var size = S2_composite.size()
+print('Number of images in collection:',size, dates);
 
 
-// display each image in collection  
-
-var visParams = {
-  bands: ['B4', 'B3', 'B2'],
-  min: 0,
-  max: 3000,
-  gamma: 1.4,
-};
-
-  
-// client side loop
+// Display each image in collection  
 var listOfImages = S2_composite.toList(S2_composite.size());
+var visParams = {bands:['B4','B3','B2'],min:0,max:0.5,gamma:1}; 
 
-for(var i = 0; i < 7; i++){
+for(var i = 0; i < size.getInfo(); i++){
   var image = ee.Image(listOfImages.get(i));
-  print(image)
-  Map.addLayer(image, visParams, i.toString())
+  var image = image.clip(geometry);
+  print()
+  Map.addLayer(image, visParams, i.toString())  
 }
 
 
