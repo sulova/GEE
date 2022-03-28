f70e74258dc4cdc0ca7e8b381f9b8842e6f6cf8e
Andrea Sulova <44543964+sulova@users.noreply.github.com>
2022-03-28 16:10:27 +0200

    Update 2_SandWave.js

--- 2_SandWave.js
+++ 2_SandWave.js
@@ -1,13 +1,13 @@
 var s2_20150809T103438 = ee.Image('COPERNICUS/S2/20150809T103438_20160715T140410_T32UME');
 var s2_20160826T104022 = ee.Image('COPERNICUS/S2/20160826T104022_20160826T104023_T32UME');
 var s2_20160925T104022 = ee.Image('COPERNICUS/S2/20160925T104022_20160925T104115_T32UME');
-var s2_20180520T103019 = ee.Image('COPERNICUS/S2/20180520T103019_20180520T103458_T32UME');
-var s2_20180806T104021 = ee.Image('COPERNICUS/S2/20180806T104021_20180806T104340_T32UME');
-var s2_20200529T102559 = ee.Image('COPERNICUS/S2/20200529T102559_20200529T103145_T32UME');
-var s2_20200601T103629 = ee.Image('COPERNICUS/S2/20200601T103629_20200601T104439_T32UME');
-var s2_20201108T104249 = ee.Image('COPERNICUS/S2/20201108T104249_20201108T104243_T32UME');
-var s2_20210305T102809 = ee.Image('COPERNICUS/S2/20210305T102809_20210305T103123_T32UME');
-var s2_20220325T102651 = ee.Image('COPERNICUS/S2/20220325T102651_20220325T103203_T32UME');
+var s2_20180520T103019 = ee.Image('COPERNICUS/S2_SR_HARMONIZED/20180520T103019_20180520T103458_T32UME');
+var s2_20180806T104021 = ee.Image('COPERNICUS/S2_SR_HARMONIZED/20180806T104021_20180806T104340_T32UME');
+var s2_20200529T102559 = ee.Image('COPERNICUS/S2_SR_HARMONIZED/20200529T102559_20200529T103145_T32UME');
+var s2_20200601T103629 = ee.Image('COPERNICUS/S2_SR_HARMONIZED/20200601T103629_20200601T104439_T32UME');
+var s2_20201108T104249 = ee.Image('COPERNICUS/S2_SR_HARMONIZED/20201108T104249_20201108T104243_T32UME');
+var s2_20210305T102809 = ee.Image('COPERNICUS/S2_SR_HARMONIZED/20210305T102809_20210305T103123_T32UME');
+var s2_20220325T102651 = ee.Image('COPERNICUS/S2_SR_HARMONIZED/20220325T102651_20220325T103203_T32UME');
 
 var s2_20150809T103438 = s2_20150809T103438 .clip(geometry);
 var s2_20160826T104022 = s2_20160826T104022 .clip(geometry);
@@ -45,28 +45,26 @@ Export.image.toDrive({  image: s2_20160925T104022.select("B4", "B3", "B2"),
   description: 's2_20160925T104022',  scale: 10,  region: geometry}); 
 
 Export.image.toDrive({  image: s2_20220325T102651.select("B4", "B3", "B2"),
-  description: 's2_20220325T102651',  scale: 10,  region: geometry});    
-
+  description: 's2_20220325T102651_H',  scale: 10,  region: geometry});    
 
 Export.image.toDrive({  image: s2_20210305T102809.select("B4", "B3", "B2"),
-  description: 's2_20210305T102809',  scale: 10,  region: geometry});    
+  description: 's2_20210305T102809_H',  scale: 10,  region: geometry});    
 
 Export.image.toDrive({  image: s2_20201108T104249.select("B4", "B3", "B2"),
-  description: 's2_20201108T104249',  scale: 10,  region: geometry});    
-
+  description: 's2_20201108T104249_H',  scale: 10,  region: geometry});    
 
 Export.image.toDrive({  image: s2_20200601T103629.select("B4", "B3", "B2"),
-  description: 's2_20200601T103629',  scale: 10,
+  description: 's2_20200601T103629_H',  scale: 10,
   region: geometry});    
 
 Export.image.toDrive({  image: s2_20200529T102559.select("B4", "B3", "B2"),
-  description: 's2_20200529T102559',  scale: 10,  region: geometry});    
+  description: 's2_20200529T102559_H',  scale: 10,  region: geometry});    
   
 Export.image.toDrive({  image: s2_20180806T104021.select("B4", "B3", "B2"),
-  description: 's2_20180806T104021',  scale: 10,  region: geometry});    
+  description: 's2_20180806T104021_H',  scale: 10,  region: geometry});    
 
 Export.image.toDrive({  image: s2_20180520T103019.select("B4", "B3", "B2"),
-  description: 's2_20180520T103019',  scale: 10,  region: geometry});    
+  description: 's2_20180520T103019_H',  scale: 10,  region: geometry});    
 
 
 
