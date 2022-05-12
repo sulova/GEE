8c3825577abf9ad4bb18d4719cfb76138097319d
Andrea Sulova <44543964+sulova@users.noreply.github.com>
2022-05-12 12:53:02 +0200

    Update 9_IR_Stat.js

--- 9_IR_Stat.js
+++ 9_IR_Stat.js
@@ -1,8 +1,8 @@
 var point =ee.Geometry.Point([-1.3, 37.7]);
 var parcels = ee.FeatureCollection("projects/geo4gras/assets/rio-segura/crop/crop_2021");
-Map.centerObject(point,8)
+Map.centerObject(point,10)
 print("Size of training parcels:",parcels.size())
-Map.addLayer(parcels, {}, 'Parcels',1);
+;
 
 var image_1 = ee.Image("projects/geo4gras/assets/rio-segura/ET/IR_20210101_20210131_sum"),
     image_2 = ee.Image("projects/geo4gras/assets/rio-segura/ET/IR_20210201_20210228_sum"),
@@ -19,18 +19,56 @@ var image_1 = ee.Image("projects/geo4gras/assets/rio-segura/ET/IR_20210101_20210
 
 var IR = ee.Image.cat([image_1,image_2,image_3,image_4,image_5,image_6,
                        image_7,image_8,image_9,image_10,image_11,image_12]);
-                       
-var IR = IR.rename(['IRsum_21M01', 'IRsum_21M02', 'IRsum_21M03', 'IRsum_21M04', 'IRsum_21M05', 'IRsum_21M06',
-                    'IRsum_21M07', 'IRsum_21M08', 'IRsum_21M09', 'IRsum_20M10', 'IRsum_20M11', 'IRsum_M2012'])         
-print(IR)
 
-                          
+var IR = IR.rename(['IRsum_21M01', 'IRsum_21M02', 'IRsum_21M03', 'IRsum_21M04', 'IRsum_21M05', 'IRsum_21M06',
+                    'IRsum_21M07', 'IRsum_21M08', 'IRsum_21M09', 'IRsum_20M10', 'IRsum_20M11', 'IRsum_M2012']) 
+                    
 var IR_mean = IR.reduceRegions({collection:parcels, reducer:ee.Reducer.mean(), scale:20, tileScale:10})                          
 
+print(IR_mean.size(), IR_mean.limit(10))
+
+var classlabels  = [
+  "Young Herbaceous irrigated in winter",21 ,
+  "Herbaceous irrigated in spring", 22,
+  "Herbaceous irrigated in summer",23 ,
+  "Herbaceous irrigated in autumn", 24,
+  "Herbaceous with double harvest (outside summer)", 31,
+  "Herbaceous with triple harvest (outside summer)", 32,
+  "Herbaceous with double harvest (including one in summer)" , 33,
+  "Herbaceous with triple harvest (including one in summer)", 34,
+  "Fruit", 1,
+  "Citrus", 2,
+  "Olivos_LAC", 3,
+  "Olivos_LBC", 4,
+  "Almonds", 5,
+  "Vineyard", 6,
+  "Vineyard (table grape)", 7,
+  "Greenhouse", 8,
+  "Young Fruit", 9,
+  "Young Citrus", 10,
+  "Low Density Tree Cover", 11,
+  "Family gardens",12]
+
+var classlabels = ee.Dictionary(classlabels)
+
+var parcels = parcels.filter(ee.Filter.inList('Seasons_T', classlabels.keys()))
+                // .filter(ee.Filter.notNull(['Q2_B2', 'Q3_B2']))
+                .map(function(f){return f.set('label', classlabels.get(f.get('Seasons_T')))})
+print('parcel size by class', parcels.aggregate_histogram('label'))
+
+var parcels = parcels.filter(ee.Filter.inList('Seasons_T', classlabels.keys()))
+                // .filter(ee.Filter.notNull(['Q2_B2', 'Q3_B2']))
+                .map(function(f){return f.set('label', classlabels.get(f.get('Seasons_T')))})
+print('Translate size by class', parcels.aggregate_histogram('Seasons_T'))
 
 //____MAPs_______
+var vis =  {min: 0,  max: 1, palette: ['FCFDBF', '2C105C']}
+var bandNames = IR.bandNames(); 
 
-var IRsum_21M01 = IR.select('IRsum_21M01')
-var IRsum_21M02 = IR.select('IRsum_21M02')
-Map.addLayer(IRsum_21M01, {min: 1,  max: 3, palette: ['FCFDBF', '711F81', '2C105C']},"IRsum_21M01",1)
-Map.addLayer(IRsum_21M02, {min: 1,  max: 3, palette: ['FCFDBF', 'FDAE78', 'EE605E', 'B63679', '711F81', '2C105C']},"IRsum_21M02",1)
+for(var i = 0; i < bandNames.size().getInfo(); i++){
+  var image = ee.Image(IR.select(bandNames.getString(i)));
+  var name = bandNames.getString(i).getInfo()
+  Map.addLayer(image, vis, name.toString(),  true)}
+
+Map.addLayer(parcels, {}, 'Parcels',0)
+  
